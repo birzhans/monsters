27ed752ecc8c6e267c2c712599aa1fa33f846167
Birzhan Serik <180107167@stu.sdu.edu.kz>
2021-01-21 16:29:16 +0600

    commit

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import './App.css';
+import { Component } from 'react';
+import { CardList } from './components/card-list/CardList';
+import { SearchBox } from './components/search-box/searchBox';
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      monsters: [],
+      searchField: ''
+    }
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => this.setState({ monsters: users }))
+  }
+
+  render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter(monster =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    );
+    
+    return (
+      <div className="App">
+        <h1>Monsters Rolodex</h1>
+        <SearchBox 
+          placeholder="Search Monsters" 
+          handleChange={e => this.setState({ searchField: e.target.value })} />
+        <CardList monsters={filteredMonsters} />
+      </div>
+    )
+  }
+}
+
+export default App;
